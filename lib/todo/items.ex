55fc6800f20072693b5212f6b820c88a7d113977
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-04-07 22:43:20 -0400

    create Todo business logic

--- lib/todo/items.ex
+++ lib/todo/items.ex
@@ -1,3 +1,51 @@
 defmodule Todo.Items do
+  alias Todo.Items.Item
+  alias Todo.Repo
+  import Ecto.Query
 
+  def get_item(id) do
+    Repo.get(Item, id)
+  end
+
+  def list_items() do
+    # query = Item
+    # |> order_by(desc: :id)
+    # Repo.all(query)
+
+    # refactored into
+
+    Item
+    |> order_by(desc: :id)
+    |> Repo.all()
+  end
+
+  def mark_completed(id) do
+    # item = Repo.get(Item, id)
+    # item = Ecto.Changeset.change item, completed: true
+    # Repo.update(item)
+
+    # refactored into
+
+    Item
+    |> Repo.get(id)
+    |> Ecto.Changeset.change(completed: true)
+    |> Repo.update()
+  end
+
+  def delete_item(id) do
+    # item = Repo.get(Item, id)
+    # Repo.delete(item)
+
+    # refactored into
+
+    Item
+    |> Repo.get(id)
+    |> Repo.delete()
+  end
+
+  def create_item(params) do
+    %Item{}
+    |> Item.changeset(params)
+    |> Repo.insert()
+  end
 end
