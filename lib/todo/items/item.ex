2fc0a5f78aae9f68b63e46caa45008c48ccb4a2b
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-04-07 22:34:13 -0400

    create schema

---
+++ lib/todo/items/item.ex
@@ -0,0 +1,21 @@
+defmodule Todo.Items.Item do
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "items" do
+    field :title, :string
+    field :description, :string
+    field :completed, :boolean, default: false
+
+    timestamps()
+  end
+
+  @doc """
+  changeset validates data before inserting into the database.
+  """
+  def changeset(item, params) do
+    item
+    |> cast(params, [:title, :description, :completed])
+  end
+end
