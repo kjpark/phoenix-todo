675c00901c5a7806ce845c22acec32c468d06742
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-04-11 22:14:24 -0400

    return success or error when creating an item

--- lib/todo_web/controllers/items_controller.ex
+++ lib/todo_web/controllers/items_controller.ex
@@ -11,8 +11,30 @@ defmodule TodoWeb.ItemsController do
   end
 
   def create(conn, %{"item" => item_params}) do
-    Items.create_item(item_params)
-    redirect(conn, to: "/items")
+    # Items.create_item(item_params)
+    # redirect(conn, to: "/items")
+
+    case Items.create_item(item_params) do
+    {:ok, item} ->
+      conn
+      |> put_flash(:info, "Successfully created: \"#{item.title}\"")
+      |> redirect(to: Routes.items_path(conn, :index))
+
+    {:error, %Ecto.Changeset{} = changeset} ->
+      # https://elixirforum.com/t/how-to-print-changeset-errors-without-using-the-changeset-in-form-for/24106
+      errors = changeset
+      |> Ecto.Changeset.traverse_errors(fn {msg, opts} ->
+        Enum.reduce(opts, msg, fn {key, value}, acc ->
+          String.replace(acc, "%{#{key}}", to_string(value))
+        end)
+      end)
+      |> Enum.map(fn {key, errors} -> "#{key}: #{Enum.join(errors, ", ")}" end)
+      |> Enum.join("\n")
+
+      conn
+      |> put_flash(:error, "Error: #{errors}")
+      |> redirect(to: Routes.items_path(conn, :index))
+    end
   end
 
   def complete(conn, %{"id" => id}) do
